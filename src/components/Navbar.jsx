8d13c0efeb99d817a0ac6896e2da5b7b1ffa946e
Khushi Bhardwaj <khushibhardwaj@Khushis-MacBook-Air.local>
2024-05-19 16:40:23 -0700

    commit message

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import { FaShoppingCart } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
+
+
+
+const Navbar=()=>{
+  return(
+    <div>
+      <div className="flex flex-row justify-between">
+        <NavLink to="/">
+          <div>
+          <img src="../logo.png"  />
+          </div>
+        </NavLink>
+        <div>
+          <NavLink>
+          <p>Home</p>
+          </NavLink>
+          <NavLink>
+            <div>
+            <FaShoppingCart />
+            </div>
+          </NavLink>
+         
+        </div>
+
+      </div>
+    </div>
+  );
+
+}
+
+
+export default Navbar;
